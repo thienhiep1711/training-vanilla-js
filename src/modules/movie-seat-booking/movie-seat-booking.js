e0a0acc1efcc6137a550382f06f6deae3b4395c5
Hiep Nguyen <thienhiep171193@gmail.com>
2021-02-28 11:18:24 +0700

    PVJ-2 Update logic and style for Movie seat booking

--- src/modules/movie-seat-booking/movie-seat-booking.js
+++ src/modules/movie-seat-booking/movie-seat-booking.js
@@ -1,14 +1,44 @@
+/* global localStorage */
+
 export default (el) => {
   const ticketPrice = el.querySelector('#movie')
   const container = el.querySelector('.container')
+  const seats = el.querySelectorAll('.row .seat:not(.occupied)')
   const ticketCount = el.querySelector('#count')
   const ticketTotal = el.querySelector('#total')
-  const showCase = el.querySelector('.showcase')
+
+  const populateUI = () => {
+    const selectedSeats = localStorage.getItem('selectedSeats')
+    if (selectedSeats && selectedSeats.length > 0) {
+      seats.forEach((seat, index) => {
+        if (selectedSeats.indexOf(index) > -1) {
+          seat.classList.add('selected')
+        }
+      })
+    }
+
+    const selectedMovieIndexs = localStorage.getItem('selectedMovieIndexs')
+    if (selectedMovieIndexs !== null) {
+      ticketPrice.selectedIndex = selectedMovieIndexs
+    }
+  }
+
+  populateUI()
 
   let ticketPriceValue = ticketPrice.value
 
+  const setMovieData = (movieIndex, moviePrice) => {
+    localStorage.setItem('selectedMovieIndexs', movieIndex)
+    localStorage.setItem('selectedMoviePrice', moviePrice)
+  }
+
   const updateSelectedCount = () => {
     const seletedSeats = container.querySelectorAll('.row .seat.selected')
+    const seatsIndex = [...seletedSeats].map((item) => {
+      return [...seats].indexOf(item)
+    })
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
     const selectedSeatsCount = seletedSeats.length
     ticketCount.innerText = selectedSeatsCount
     ticketTotal.innerText = selectedSeatsCount * ticketPriceValue
@@ -16,6 +46,7 @@ export default (el) => {
 
   ticketPrice.addEventListener('change', (e) => {
     ticketPriceValue = e.target.value
+    setMovieData(e.target.selectedIndex, e.target.value)
     updateSelectedCount()
   })
 
@@ -26,5 +57,6 @@ export default (el) => {
         updateSelectedCount()
       }
     })
+    updateSelectedCount()
   }
 }
