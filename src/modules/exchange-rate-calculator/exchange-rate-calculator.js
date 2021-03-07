b28ccd92d2e1929b6d32a493cda3934f2eabdd85
Hiep Nguyen <thienhiep171193@gmail.com>
2021-03-07 13:07:49 +0700

    PVJ-4 Update function calculator in exchange rate calculator module

---
+++ src/modules/exchange-rate-calculator/exchange-rate-calculator.js
@@ -0,0 +1,39 @@
+/* global fetch */
+
+export default (el) => {
+  if (el) {
+    const currencyBase = document.getElementById('currency-one')
+    const currencyBaseValue = document.getElementById('amount-one')
+    const currencyExchange = document.getElementById('currency-two')
+    const currencyExchangeValue = document.getElementById('amount-two')
+    const rateEl = document.getElementById('rate')
+    const swap = document.getElementById('swap')
+
+    const calculator = () => {
+      let currentBase = currencyBase.value
+      let exchangeBase = currencyExchange.value
+
+      fetch(`https://api.exchangerate.host/latest?base=${currentBase}`)
+        .then(res => res.json())
+        .then((data) => {
+          if (data) {
+            const rate = data.rates[exchangeBase]
+            rateEl.innerText = `1 ${currentBase} = ${rate} ${exchangeBase}`
+            currencyExchangeValue.value = (currencyBaseValue.value * rate).toFixed(2)
+          }
+        })
+    }
+
+    currencyBase.addEventListener('change', calculator)
+    currencyBaseValue.addEventListener('input', calculator)
+    currencyExchange.addEventListener('change', calculator)
+    currencyExchangeValue.addEventListener('input', calculator)
+
+    swap.addEventListener('click', () => {
+      const temp = currencyBase.value
+      currencyBase.value = currencyExchange.value
+      currencyExchange.value = temp
+      calculator()
+    })
+  }
+}
